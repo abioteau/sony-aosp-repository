5e0b17784473a0262ea7c545139411244b34cea9
Adrien Bioteau <adrien.bioteau@gmail.com>
2016-09-19 12:13:00 +0200

    Get Sony Xperia software binaries to build AOSP

--- script/download.sh
+++ script/download.sh
@@ -4,14 +4,12 @@
 # Permission to copy and modify is granted under the GPLv3 license
 # Last revised 09/12/2016
 
-rm -rf sonyxperiadev
 mkdir -p sonyxperiadev
 
 # Get AOSP Kitkat Instruction for Sony Mobile web page
 mkdir -p orig/kitkat
 wget "http://developer.sonymobile.com/open-devices/aosp-build-instructions/how-to-build-aosp-kitkat-for-unlocked-xperia-devices/" -O orig/kitkat/index.html
 cat orig/kitkat/index.html | sed -n '/<article class="article page-article"/,$p' | sed '/article>/q' > sonyxperiadev/build-aosp-kitkat-4.4.html
-rm -rf orig/kitkat
 
 # Get AOSP Lollipop Instruction for Sony Mobile web page
 mkdir -p orig/lollipop
@@ -19,7 +17,6 @@ wget "http://developer.sonymobile.com/open-devices/aosp-build-instructions/how-t
 cat orig/lollipop/index.html.tmp | sed -n '/<div class="section overview-section faq-overview-section"/,$p' | sed '/div>/q' > orig/lollipop/index.html
 cat orig/lollipop/index.html | sed -n '/<dt id="build-aosp-lollipop-5-1"/,$p' | sed '/\/dd>/q' > sonyxperiadev/build-aosp-lollipop-5.1.html
 cat orig/lollipop/index.html | sed -n '/<dt id="build-aosp-lollipop-5-0"/,$p' | sed '/\/dd>/q' > sonyxperiadev/build-aosp-lollipop-5.0.html
-rm -rf orig/lollipop
 
 # Get AOSP Marshmallow Instruction for Sony Mobile web page
 mkdir -p orig/marshmallow
@@ -27,16 +24,14 @@ wget "http://developer.sonymobile.com/open-devices/aosp-build-instructions/how-t
 cat orig/marshmallow/index.html.tmp | sed -n '/<div class="section overview-section faq-overview-section"/,$p' | sed '/div>/q' > orig/marshmallow/index.html
 cat orig/marshmallow/index.html | sed -n '/<dt id="build-aosp-marshmallow-experimental"/,$p' | sed '/\/dd>/q' > sonyxperiadev/build-aosp-marshmallow-6.0.1.html
 cat orig/marshmallow/index.html | sed -n '/<dt id="build-aosp-marshmallow-6-0"/,$p' | sed '/\/dd>/q' > sonyxperiadev/build-aosp-marshmallow-6.0.html
-rm -rf orig/marshmallow
 
 # Get AOSP Nougat Instruction for Sony Mobile web page
 mkdir -p orig/nougat
 wget "http://developer.sonymobile.com/open-devices/aosp-build-instructions/how-to-build-aosp-nougat-for-unlocked-xperia-devices/" -O orig/nougat/index.html.tmp
 cat orig/nougat/index.html.tmp | sed -n '/<div class="section overview-section faq-overview-section"/,$p' | sed '/div>/q' > orig/nougat/index.html
 cat orig/nougat/index.html | sed -n '/<dt id="build-aosp-nougat-7-0"/,$p' | sed '/\/dd>/q' > sonyxperiadev/build-aosp-nougat-7.0.html
-rm -rf orig/nougat
 
-buildInstructions=`find sonyxperiadev/*.html`
+buildInstructions=`find sonyxperiadev/build-aosp-*.html`
 
 for file in ${buildInstructions};
 do
@@ -48,21 +43,46 @@ do
 
 	grep "repo init" ${file} | grep -o -E "android-[0-9._r]*" > ${outdir}/AOSP_TAG
 
-	cat ${file} |
-		sed 's/<br \/>//g' |
-		sed 's/&lt;/\</g' |
-		sed 's/&quot;/\"/g' |
-		sed 's/&gt;/\>/g' |
-		sed 's/.*<?xml version/<?xml version/' |
-		sed 's/\/manifest>.*/\/manifest>/' |
+	cat ${file} | \
+		sed 's/<br \/>//g' | \
+		sed 's/&lt;/\</g' | \
+		sed 's/&quot;/\"/g' | \
+		sed 's/&gt;/\>/g' | \
+		sed 's/.*<?xml version/<?xml version/g' | \
+		sed 's/\/manifest>.*/\/manifest>/g' | \
 		sed -n '/<?xml version/,$p' | sed '/\/manifest>/q' > ${outdir}/sony.xml
 
-	cat ${file} |
-		sed 's/<br \/>//g' |
-		sed 's/.*cd /cd /g' |
-		sed 's/\/li>.*/\/li>/g' |
-		sed -n '/cd [build|hardware|external|system|packages]/,$p' | sed '/\/li\>/q' |
-		sed 's/<\/pre>//g' |
-		sed 's/<\/li>//g' |
+	cat ${file} | \
+		sed 's/<br \/>//g' | \
+		sed 's/.*cd /cd /g' | \
+		sed 's/\/li>.*/\/li>/g' | \
+		sed -n '/cd [build|hardware|external|system|packages]/,$p' | sed '/\/li\>/q' | \
+		sed 's/<\/pre>//g' | \
+		sed 's/<\/li>//g' | \
 		sed 's/<\/code>//g' > ${outdir}/AOSP_PATCH
 done
+
+# Get AOSP software binaries for Sony Mobile web page
+mkdir -p orig/binaries
+wget "http://developer.sonymobile.com/downloads/software-binaries/" -O orig/binaries/index.html
+cat orig/binaries/index.html | sed -n '/<div class="section downloads-section"/,$p' | sed '/div>/q' > sonyxperiadev/software-binaries.html
+
+mkdir -p sonyxperiadev/binaries
+cat orig/binaries/index.html | sed -n '/<tbody/,$p' | sed '/\/tbody>/q' > orig/binaries/body.html
+binariesNumber=`cat orig/binaries/body.html | grep -c "<tr>"`
+counter=0
+while [[ ${counter} < ${binariesNumber} ]];
+do
+	cat orig/binaries/body.html | sed -n '/<tr/,$p' | sed '/\/tr>/q' > orig/binaries/body-${counter}.html
+
+	grep -o 'http://dl-developer.sonymobile.com/eula[^"]*' orig/binaries/body-${counter}.html | grep AOSP | \
+		xargs -I {} wget {} -O orig/binaries/eula-${counter}.html
+	grep -o "restricted/[^\']*" orig/binaries/eula-${counter}.html | sed 's/\?param=//g' | sed 's/restricted\///g' | \
+		xargs -I {} wget -c --no-cookies --header "Cookie: dw_accepted=true" "http://dl-developer.sonymobile.com/eula/restricted/"{} -O sonyxperiadev/binaries/{}
+
+	cat orig/binaries/body.html | sed -n '/\/tr>/,$p' | sed '1s/<\/tr>//g' > orig/binaries/body.html.tmp
+	cp orig/binaries/body.html.tmp orig/binaries/body.html
+	counter=$((counter+1))
+done
+
+rm -rf orig
