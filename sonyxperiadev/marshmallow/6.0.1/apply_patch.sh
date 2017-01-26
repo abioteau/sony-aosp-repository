84d92c1cdd8b5a31312b565353f846e0c62cea41
Adrien Bioteau <adrien.bioteau@gmail.com>
2017-01-26 21:26:27 +0000

    Sony Xperia AOSP build instructions 01/26/2017

--- sonyxperiadev/marshmallow/6.0.1/apply_patch.sh
+++ sonyxperiadev/marshmallow/6.0.1/apply_patch.sh
@@ -21,7 +21,7 @@ GIT_BRANCH=$5
 
 mkdir -p $AOSP_WORKSPACE
 cd $AOSP_WORKSPACE
-~/bin/repo init -u $AOSP_MIRROR_URL/platform/manifest.git --repo-url $REPO_MIRROR_URL/git-repo.git -b android-6.0.1_r77
+~/bin/repo init -u $AOSP_MIRROR_URL/platform/manifest.git --repo-url $REPO_MIRROR_URL/git-repo.git -b android-6.0.1_r78
 cp $ROOTDIR/sonyxperiadev/marshmallow/6.0.1/sony.xml .repo/manifests/sony.xml
 sed -i "s/fetch=\".*\"/fetch=\"$SONY_MIRROR_URL\"/" .repo/manifests/sony.xml
 sed -i "/^<project/ s/name=\"/name=\"sonyxperiadev\//" .repo/manifests/sony.xml
@@ -66,6 +66,6 @@ cd ../../../../../
 
 
 ~/bin/repo status
-~/bin/repo forall -p -c git log --oneline android-6.0.1_r77..$GIT_BRANCH
+~/bin/repo forall -p -c git log --oneline android-6.0.1_r78..$GIT_BRANCH
 
 cd $ROOTDIR
