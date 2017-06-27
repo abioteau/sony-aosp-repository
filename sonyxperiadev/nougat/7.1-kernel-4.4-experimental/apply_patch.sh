d9e3902143d753bb08a5233f32dd4c85f972b167
Adrien Bioteau <adrien.bioteau@gmail.com>
2017-06-27 14:47:11 +0000

    Sony Xperia AOSP build instructions 06/27/2017

---
+++ sonyxperiadev/nougat/7.1-kernel-4.4-experimental/apply_patch.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Script to apply Sony Xperia patches
+# Copyright (C) 2017 Adrien Bioteau - All Rights Reserved
+# Permission to copy and modify is granted under the GPLv3 license
+# Last revised 05/17/2017
+
+cd `dirname $0`/../../..
+ROOTDIR=`pwd`
+NB_CORES=`grep -c ^processor /proc/cpuinfo`
+
+if [ $# -ne 5 ]
+then
+    echo "[USAGE] ./apply_patch.sh <aosp_workspace> <aosp_mirror> <repo_mirror> <github_mirror> <git_branch>"
+    exit 1
+fi
+
+AOSP_WORKSPACE=$1
+AOSP_MIRROR_URL=$2
+REPO_MIRROR_URL=$3
+GITHUB_MIRROR_URL=$4
+GIT_BRANCH=$5
+
+mkdir -p $AOSP_WORKSPACE
+cd $AOSP_WORKSPACE
+~/bin/repo init -u $AOSP_MIRROR_URL/platform/manifest.git --repo-url $REPO_MIRROR_URL/git-repo.git -b android-7.1.2_r17
+
+sed -i "/^<\/manifest/ s/\(.*\)/  <!-- Sony AOSP addons -->\n\1/" .repo/manifests/default.xml
+git clone $GITHUB_MIRROR_URL/sonyxperiadev/local_manifests
+cd local_manifests
+git checkout -f n-mr1_4.4
+sed -i "s/fetch=\".*:\/\/github.com\/\(.*\)\"/fetch=\"$(echo $GITHUB_MIRROR_URL | sed 's/\//\\\//g')\/\1\"/" *.xml
+find *.xml | xargs -I {} sed -i "/^<\/manifest/ s/\(.*\)/  <include name=\"{}\"\/>\n\1/" ../.repo/manifests/default.xml
+cp *.xml ../.repo/manifests/.
+cd ..
+rm -rf local_manifests
+git clone $GITHUB_MIRROR_URL/abioteau/vendor_manifests
+cd vendor_manifests
+git checkout -f n-mr1_4.4
+sed -i "s/fetch=\".*:\/\/github.com\/\(.*\)\"/fetch=\"$(echo $GITHUB_MIRROR_URL | sed 's/\//\\\//g')\/\1\"/" *.xml
+find *.xml | xargs -I {} sed -i "/^<\/manifest/ s/\(.*\)/  <include name=\"{}\"\/>\n\1/" ../.repo/manifests/default.xml
+cp *.xml ../.repo/manifests/.
+cd ..
+rm -rf vendor_manifests
+
+~/bin/repo sync -j $NB_CORES
+~/bin/repo manifest -o manifest.xml -r
+
+cd external/toybox && repo start $GIT_BRANCH .
+git fetch http://android.googlesource.com/platform/external/toybox refs/changes/74/265074/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git format-patch -o /tmp/d3e8dd1bf56afc2277960472a46907d419e4b3da -1 d3e8dd1bf56afc2277960472a46907d419e4b3da && git am -3 --committer-date-is-author-date /tmp/d3e8dd1bf56afc2277960472a46907d419e4b3da/0001-*.patch && rm -rf /tmp/d3e8dd1bf56afc2277960472a46907d419e4b3da
+git format-patch -o /tmp/1c028ca33dc059a9d8f18daafcd77b5950268f41 -1 1c028ca33dc059a9d8f18daafcd77b5950268f41 && git am -3 --committer-date-is-author-date /tmp/1c028ca33dc059a9d8f18daafcd77b5950268f41/0001-*.patch && rm -rf /tmp/1c028ca33dc059a9d8f18daafcd77b5950268f41
+git format-patch -o /tmp/cb49c305e3c78179b19d6f174ae73309544292b8 -1 cb49c305e3c78179b19d6f174ae73309544292b8 && git am -3 --committer-date-is-author-date /tmp/cb49c305e3c78179b19d6f174ae73309544292b8/0001-*.patch && rm -rf /tmp/cb49c305e3c78179b19d6f174ae73309544292b8
+cd ../libnfc-nci && repo start $GIT_BRANCH .
+git fetch http://android.googlesource.com/platform/external/libnfc-nci refs/changes/52/371052/1 &amp;&amp; git cherry-pick FETCH_HEAD
+cd ../../hardware/qcom/bt && repo start $GIT_BRANCH .
+git fetch http://android.googlesource.com/platform/hardware/qcom/bt refs/changes/99/422499/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/hardware/qcom/bt refs/changes/00/422500/1 &amp;&amp; git cherry-pick FETCH_HEAD
+cd ../audio && repo start $GIT_BRANCH .
+git fetch http://android.googlesource.com/platform/hardware/qcom/audio refs/changes/91/294291/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/hardware/qcom/audio refs/changes/35/274235/9 &amp;&amp; git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/hardware/qcom/audio refs/changes/86/333386/1 &amp;&amp; git cherry-pick FETCH_HEAD
+cd ../media && repo start $GIT_BRANCH .
+git fetch http://android.googlesource.com/platform/hardware/qcom/media refs/changes/39/422439/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/hardware/qcom/media refs/changes/79/422379/1 &amp;&amp; git cherry-pick FETCH_HEAD
+cd ../../../system/core && repo start $GIT_BRANCH .
+git fetch http://android.googlesource.com/platform/system/core refs/changes/52/269652/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git fetch http://android.googlesource.com/platform/system/core refs/changes/12/373812/1 &amp;&amp; git cherry-pick FETCH_HEAD
+cd ../../packages/apps/Music && repo start $GIT_BRANCH .
+git format-patch -o /tmp/6036ce6127022880a3d9c99bd15db4c968f3e6a3 -1 6036ce6127022880a3d9c99bd15db4c968f3e6a3 && git am -3 --committer-date-is-author-date /tmp/6036ce6127022880a3d9c99bd15db4c968f3e6a3/0001-*.patch && rm -rf /tmp/6036ce6127022880a3d9c99bd15db4c968f3e6a3
+cd ../../../frameworks/av && repo start $GIT_BRANCH .
+git fetch http://android.googlesource.com/platform/frameworks/av refs/changes/92/384692/2 &amp;&amp; git cherry-pick FETCH_HEAD
+cd ../../
+
+
+
+cd $ROOTDIR
