78e24ef83913d74bb5f59120091e7ec8aa4205fb
Adrien Bioteau <adrien.bioteau@gmail.com>
2017-06-29 06:38:45 +0000

    Sony Xperia AOSP build instructions 06/29/2017

--- sonyxperiadev/nougat/7.1-kernel-4.4/apply_patch.sh
+++ sonyxperiadev/nougat/7.1-kernel-4.4/apply_patch.sh
@@ -46,29 +46,29 @@ rm -rf vendor_manifests
 ~/bin/repo manifest -o manifest.xml -r
 
 cd external/toybox && repo start $GIT_BRANCH .
-git fetch http://android.googlesource.com/platform/external/toybox refs/changes/74/265074/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/external/toybox/refs/changes/74/265074/1/*.patch`
 git format-patch -o /tmp/d3e8dd1bf56afc2277960472a46907d419e4b3da -1 d3e8dd1bf56afc2277960472a46907d419e4b3da && git am -3 --committer-date-is-author-date /tmp/d3e8dd1bf56afc2277960472a46907d419e4b3da/0001-*.patch && rm -rf /tmp/d3e8dd1bf56afc2277960472a46907d419e4b3da
 git format-patch -o /tmp/1c028ca33dc059a9d8f18daafcd77b5950268f41 -1 1c028ca33dc059a9d8f18daafcd77b5950268f41 && git am -3 --committer-date-is-author-date /tmp/1c028ca33dc059a9d8f18daafcd77b5950268f41/0001-*.patch && rm -rf /tmp/1c028ca33dc059a9d8f18daafcd77b5950268f41
 git format-patch -o /tmp/cb49c305e3c78179b19d6f174ae73309544292b8 -1 cb49c305e3c78179b19d6f174ae73309544292b8 && git am -3 --committer-date-is-author-date /tmp/cb49c305e3c78179b19d6f174ae73309544292b8/0001-*.patch && rm -rf /tmp/cb49c305e3c78179b19d6f174ae73309544292b8
 cd ../libnfc-nci && repo start $GIT_BRANCH .
-git fetch http://android.googlesource.com/platform/external/libnfc-nci refs/changes/52/371052/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/external/libnfc-nci/refs/changes/52/371052/1/*.patch`
 cd ../../hardware/qcom/bt && repo start $GIT_BRANCH .
-git fetch http://android.googlesource.com/platform/hardware/qcom/bt refs/changes/99/422499/1 &amp;&amp; git cherry-pick FETCH_HEAD
-git fetch http://android.googlesource.com/platform/hardware/qcom/bt refs/changes/00/422500/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/hardware/qcom/bt/refs/changes/99/422499/1/*.patch`
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/hardware/qcom/bt/refs/changes/00/422500/1/*.patch`
 cd ../audio && repo start $GIT_BRANCH .
-git fetch http://android.googlesource.com/platform/hardware/qcom/audio refs/changes/91/294291/1 &amp;&amp; git cherry-pick FETCH_HEAD
-git fetch http://android.googlesource.com/platform/hardware/qcom/audio refs/changes/35/274235/9 &amp;&amp; git cherry-pick FETCH_HEAD
-git fetch http://android.googlesource.com/platform/hardware/qcom/audio refs/changes/86/333386/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/hardware/qcom/audio/refs/changes/91/294291/1/*.patch`
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/hardware/qcom/audio/refs/changes/35/274235/9/*.patch`
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/hardware/qcom/audio/refs/changes/86/333386/1/*.patch`
 cd ../media && repo start $GIT_BRANCH .
-git fetch http://android.googlesource.com/platform/hardware/qcom/media refs/changes/39/422439/1 &amp;&amp; git cherry-pick FETCH_HEAD
-git fetch http://android.googlesource.com/platform/hardware/qcom/media refs/changes/79/422379/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/hardware/qcom/media/refs/changes/39/422439/1/*.patch`
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/hardware/qcom/media/refs/changes/79/422379/1/*.patch`
 cd ../../../system/core && repo start $GIT_BRANCH .
-git fetch http://android.googlesource.com/platform/system/core refs/changes/52/269652/1 &amp;&amp; git cherry-pick FETCH_HEAD
-git fetch http://android.googlesource.com/platform/system/core refs/changes/12/373812/1 &amp;&amp; git cherry-pick FETCH_HEAD
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/system/core/refs/changes/52/269652/1/*.patch`
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/system/core/refs/changes/12/373812/1/*.patch`
 cd ../../packages/apps/Music && repo start $GIT_BRANCH .
 git format-patch -o /tmp/6036ce6127022880a3d9c99bd15db4c968f3e6a3 -1 6036ce6127022880a3d9c99bd15db4c968f3e6a3 && git am -3 --committer-date-is-author-date /tmp/6036ce6127022880a3d9c99bd15db4c968f3e6a3/0001-*.patch && rm -rf /tmp/6036ce6127022880a3d9c99bd15db4c968f3e6a3
 cd ../../../frameworks/av && repo start $GIT_BRANCH .
-git fetch http://android.googlesource.com/platform/frameworks/av refs/changes/92/384692/2 &amp;&amp; git cherry-pick FETCH_HEAD
+git am -3 --committer-date-is-author-date `ls $ROOTDIR/sonyxperiadev/patches/platform/frameworks/av/refs/changes/92/384692/2/*.patch`
 cd ../../
 
 
